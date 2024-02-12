cd3032c921402ec5459fc3d24478805de1cc93ab
Kelvin Doe <kelvindoe22@gmail.com>
2024-02-12 01:21:55 +0000

    set thread count i'll handle the rest

--- src/main.rs
+++ src/main.rs
@@ -12,19 +12,16 @@ use std::fs::File;
 use std::io::prelude::*;
 use std::io::{self, Write, BufRead, BufReader};
 use std::path::Path;
+use std::sync::Arc;
+use std::sync::Mutex;
 use std::thread;
 
-fn ReverseIP(ip: &str) {
+fn ReverseIP(ip: &str) -> Vec<String> {
     let url = format!("https://rapiddns.io/sameip/{}?full=1", ip);
     let response = get(url).unwrap().text().unwrap();
     let docs = Html::parse_document(&response);
     let all_td = Selector::parse("table > tbody > tr > td:nth-child(2)").unwrap();
     let mut head:Vec<String> = Vec::new();
-    let mut file = OpenOptions::new()
-        .write(true)
-        .append(true)
-        .open("res.txt")
-        .unwrap();
 
     for x in docs.select(&all_td) {
         for j in x.text() {
@@ -34,13 +31,11 @@ fn ReverseIP(ip: &str) {
 
     if head.len() != 0 {
         println!("\x1b[93m-->\x1b[0m \x1b[94m{}\x1b[0m \x1b[97mGrabbed \x1b[0m\x1b[92m{} \x1b \x1b[97mDomain\x1b", ip, head.len());
-        for m in head {
-            if let Err(e) = writeln!(file, "{}", m) {
-            }
-        }
     } else {
         println!("\x1b[93m-->\x1b[0m \x1b[94m{}\x1b[0m \x1b[97mBad IP!\x1b[0m", ip);
     }
+
+    return head;
 }
 fn Banner() {
     print!("\x1B[2J\x1B[1;1H");
@@ -64,44 +59,63 @@ fn main() {
     println!("");
 
     let mut thread_count = String::new();
-    let mut no_ips = String::new();
     print!("Enter number of threads you want to use: ");
     io::stdout().flush().unwrap();
     io::stdin().read_line(&mut thread_count).unwrap();
-    let num_of_thread = thread_count.trim().parse::<u8>().unwrap();
-    print!("Enter number of ips: ");
-    io::stdout().flush().unwrap();
-    io::stdin().read_line(&mut no_ips).unwrap();
-    let ips: usize = no_ips.trim().parse().unwrap();
+    let num_of_thread = thread_count.trim().parse::<usize>().unwrap();
 
+    
     
     let mut files = String::new();
     print!("\x1b[93m---> IP LIST : \x1b[0m");
     io::stdout().flush().unwrap();
     let b1 = std::io::stdin().read_line(&mut files).unwrap();   
-    let mut threads = Vec::with_capacity(num_of_thread as usize); 
-    if let Ok(lines) = read_lines(files.trim()) {
-        let num_of_lines_for_each_thread = (ips/num_of_thread as usize) + 1;
-        let mut lines_done = 0;
-        let mut lines_for_each_thread: Vec<String> = Vec::new();
-        for line in lines {
-            if lines_done == num_of_lines_for_each_thread {
-                let lines_for_thread = lines_for_each_thread.drain(..).collect::<Vec<_>>();
-                threads.push(thread::spawn(
-                    move || {
-                        for l in lines_for_thread {
-                            ReverseIP(&l)
+    let lines = read_lines(files.trim()).unwrap();
+    let mut threads = Vec::with_capacity(num_of_thread as usize);
+    
+    let file = OpenOptions::new()
+        .write(true)
+        .append(true)
+        .open("res.txt")
+        .unwrap();
+    let sharefiles = Arc::new(Mutex::new(file));
+
+    let sharelines = Arc::new(Mutex::new(lines));
+
+    for i in 0..num_of_thread{
+        let share = Arc::clone(&sharelines);
+        let file = sharefiles.clone();
+        threads.push(
+            thread::spawn(
+                move || {
+                    loop {
+                        let line;
+                        {
+                            line = share.lock().unwrap().next();
+                        }
+                        if line.is_none() {
+                            break;
+                        } else {
+                            if let Ok(l) =  line.as_ref().unwrap() {
+                                let domains = ReverseIP(l);
+                                if domains.len() == 0{
+                                } else {
+                                    let mut f = file.lock().unwrap();
+                                    
+                                    for d in domains {
+                                        if let Err(_) = writeln!(
+                                            f, "{}", d
+                                        ){}
+                                    }
+                                }
+                            }
                         }
                     }
-                ));
-                lines_done = 0;
-            }
-            if let Ok(s) = line {
-                lines_for_each_thread.push(s.clone());
-                lines_done += 1;
-            }
-        }
+                }
+            )
+        )
     }
+
     for t in threads {
         t.join().unwrap();
     }
